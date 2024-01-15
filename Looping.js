300d924893d70de5a6927ffa3f9301fdfb41a2e0
Edward821 <edwardbun55@gmail.com>
2024-01-15 19:13:57 +0700

    Looping

---
+++ Looping.js
@@ -0,0 +1,14 @@
+
+
+//perulangan angka 1 sampai 50 //
+for(let i=0; i<=50; i++) {
+    console.log(i)
+}
+
+//perulangan angka 50 sampai 1//
+let daun = 50
+while(daun>=1) {
+    console.log(daun)
+    daun--
+}
+
